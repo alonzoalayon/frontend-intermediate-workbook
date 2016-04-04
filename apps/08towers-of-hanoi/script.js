42d97738f2e8e3b711782a87b5b411b5b953635b
alonzoalayon <alonzo@campgladiator.com>
2016-04-04 18:47:49 -0500

    hello

--- apps/08towers-of-hanoi/script.js
+++ apps/08towers-of-hanoi/script.js
@@ -1,5 +1,48 @@
 'use strict';
-
 $(document).ready(function() {
-  // Put app logic here
-}
+    // Put app logic here
+    var towerBlock = null;
+    var $tower;
+    var $tower1;
+    var $block;
+    var $block1;
+    var $tower2 = $('#tower2');
+    var $tower3 = $('#tower3');
+    $(document).ready(init);
+
+    function init() {
+        $('div[data-stack]').on('click', selectBlock);
+    }
+
+    function selectBlock() {
+        if (towerBlock === null) {
+            $tower = $(this);
+            towerBlock = $tower.find($('div[data-block]')).data(
+                'val');
+            $block = $tower.find($('div[data-block]:last-child'));
+            if ($tower.find($('div[data-block]')).length === 0) {
+                towerBlock = null;
+            } else {
+                $block.addClass('selected');
+            }
+        } else {
+            $tower1 = $(this);
+            $block1 = $tower1.find($('div[data-block]:last-child'));
+            $block.removeClass('selected');
+            if ($block.height() < $block1.height() || $tower1.find(
+                $('div[data-block]')).length === 0) {
+                var move = $block.detach();
+                $tower1.append(move);
+                checkForWin();
+            }
+            towerBlock = null;
+        }
+    }
+
+    function checkForWin() {
+        if ($tower2.find($('div[data-block]')).length === 4 ||
+            $tower3.find($('div[data-block]')).length === 4) {
+            $('#announce-game-won').text("You Won!");
+        }
+    }
+});
