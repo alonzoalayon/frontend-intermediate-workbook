f2643d1d386c28ef6b0725d3040e726d763a18b3
alonzoalayon <alonzo@campgladiator.com>
2016-04-06 16:20:09 -0500

    Script working

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -2,4 +2,13 @@
 
 $(document).ready(function() {
     // App logic goes here
+    var todoText;
+    var $todoVar;
+    $('form').submit(function (event) { // target the form, and put a submit event listener on it. We can capture and pass in the event for us to use
+      event.preventDefault(); // this tells the browser to "prevent" the "default" "event" from occuring (sending and refreshing)
+      $todoVar = $(this);
+      todoText = $todoVar.find($('#todo')).val();
+       $('#todo-list').append('<li>' + todoText + '</li>');
+      $('#todo-list').sortable();
+    });
 });
